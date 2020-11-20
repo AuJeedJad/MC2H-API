1fc25f32918eb947075fc459bc0258103ca0b8da
koidzt <koi27792@gmail.com>
2020-11-20 20:30:55 +0700

    Fixed associate in Child,Contracepion,CurrentPrenancy,FatherProfile,Hospital,PrenantHistory,Staff

--- models/Child.js
+++ models/Child.js
@@ -1,27 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-  const Child = sequelize.define('Child', {
-    firstName: {
-      type: DataTypes.STRING
+  const Child = sequelize.define(
+    'Child',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      birthDate: {
+        type: DataTypes.DATEONLY,
+      },
+      idCard: {
+        type: DataTypes.INTEGER(13),
+      },
     },
-    lastName: {
-      type: DataTypes.STRING
-    },
-    birthDate: {
-      type: DataTypes.DATEONLY
-    },
-    idCard: {
-      type: DataTypes.INTEGER(13)
+    {
+      tableName: 'childs',
+      timestamps: true,
     }
-  }, {
-    tableName: 'childs',
-    timestamps: true
-  });
+  );
 
-  Child.associate = models => {
-    Child.hasMany(models.PregnantHistory);
+  Child.associate = (models) => {
+    Child.hasMany(models.PregnantHistory, { foreignKey: 'childId' });
     Child.belongsTo(models.FatherProfile, { foreignKey: 'fatherId' });
     Child.belongsTo(models.CurrentPregnancy, { foreignKey: 'curPregId' });
-  }
+  };
 
   return Child;
 };

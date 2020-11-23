9d3cfd865cbfcbbddfe274ca446475c6786b823f
koidzt <koi27792@gmail.com>
2020-11-23 10:21:06 +0700

    Fixed  associate in CurrentPregnancy

--- models/CurrentPregnancy.js
+++ models/CurrentPregnancy.js
@@ -42,9 +42,11 @@ module.exports = (sequelize, DataTypes) => {
   CurrentPregnancy.associate = (models) => {
     CurrentPregnancy.hasMany(models.Child, { foreignKey: 'curPregId' });
     CurrentPregnancy.hasOne(models.Contraception, { foreignKey: 'curPregId' });
+    CurrentPregnancy.hasMany(models.ANC, { foreignKey: 'curPregId' });
+    CurrentPregnancy.belongsTo(models.MotherProfile, { foreignKey: 'motherId' });
     CurrentPregnancy.hasMany(models.RiskEvaluation, { foreignKey: 'curPregId' });
-    CurrentPregnancy.hasMany(models.BabyKicking);
-    CurrentPregnancy.hasOne(models.DentalExam);
+    CurrentPregnancy.hasMany(models.BabyKicking, { foreignKey: 'curPregId' });
+    CurrentPregnancy.hasOne(models.DentalExam, { foreignKey: 'curPregId' });
   };
 
   return CurrentPregnancy;

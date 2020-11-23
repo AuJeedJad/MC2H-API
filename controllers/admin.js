6a1bf322fd0142dcc6d01c8c10279d2022248fbc
koidzt <koi27792@gmail.com>
2020-11-23 15:11:01 +0700

    formatter contriller/admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,23 +6,23 @@ const register = async (req, res) => {
     const { username, password } = req.body;
     const targetUser = await db.Staff.findOne({ where: { username } });
     if (targetUser) {
-      res.status(400).send({ message: 'Username already taken.' })
+      res.status(400).send({ message: 'Username already taken.' });
     } else {
       if (password.length < 6) {
-        res.status(400).send({ message: 'Please enter password more than 6 character' })
+        res.status(400).send({ message: 'Please enter password more than 6 character' });
       } else {
         const salt = bcryptjs.genSaltSync(Number(process.env.SALT_ROUND));
         const hashedPassword = bcryptjs.hashSync(password, salt);
         const newStaff = await db.Staff.create({
           username,
-          password: hashedPassword
+          password: hashedPassword,
         });
 
         res.status(201).send(newStaff);
       }
     }
   } catch (err) {
-    res.status(500).send({ message: err.message })
+    res.status(500).send({ message: err.message });
   }
 };
 
@@ -30,12 +30,12 @@ const resetPassword = async (req, res) => {
   try {
     const { username, setPassword } = req.body;
     if (setPassword.length < 6) {
-      return res.status(400).send({ message: 'Please enter password more than 6 character' })
+      return res.status(400).send({ message: 'Please enter password more than 6 character' });
     }
 
-    const targetUser = await db.Staff.findOne({ where: { username } })
+    const targetUser = await db.Staff.findOne({ where: { username } });
     if (!targetUser) {
-      res.status(400).send({ message: 'This username is not found' })
+      res.status(400).send({ message: 'This username is not found' });
     } else {
       const salt = bcryptjs.genSaltSync(Number(process.env.SALT_ROUND));
       const hashedPassword = bcryptjs.hashSync(setPassword, salt);
@@ -43,13 +43,12 @@ const resetPassword = async (req, res) => {
 
       res.status(200).send({ message: 'Password has been reset' });
     }
-
   } catch (err) {
-    res.status(500).send({ message: err.message })
+    res.status(500).send({ message: err.message });
   }
 };
 
 module.exports = {
   register,
-  resetPassword
+  resetPassword,
 };

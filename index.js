6e83f8249a628abc06434a4c8375b576b7f94453
Surachai Srisawang <aujeedjadss@gmail.com>
2020-11-23 15:09:09 +0700

    Setup error Middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const db = require('./models');
 const adminRoute = require('./routes/admin');
 const staffRoute = require('./routes/staff');
+const motherProfileRoute = require('./routes/motherProfile');
 
 app.use(cors());
 app.use(express.json());
@@ -13,14 +14,20 @@ app.use(express.static('public'));
 
 app.use('/admin/staffs', adminRoute);
 app.use('/staffs', staffRoute);
+app.use('/motherProfile', motherProfileRoute);
+
 app.use((req, res, next) => {
   res.status(404).send({ message: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at ${process.env.PORT}`);
 });
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   console.log('Completed Connect And Sync');
 });

736e6caa19c7b8d3bb4da6283bd28a36b02eb1c1
koidzt <koi27792@gmail.com>
2020-11-23 15:06:20 +0700

    staff create motherProfile(motherRegister)

--- controllers/staff.js
+++ controllers/staff.js
@@ -15,14 +15,43 @@ const login = async (req, res) => {
         res.status(400).send({ message: 'Username or Password is wrong.' });
       } else {
         const payload = { id: targetUser.id, createAt: new Date() };
-        const token = jwt.sign(payload, process.env.SECRET, { expiresIn: 2592000 });//token exp: 1 month
+        const token = jwt.sign(payload, process.env.SECRET, { expiresIn: 2592000 }); //token exp: 1 month
 
         res.status(200).send({ token });
       }
     }
   } catch (err) {
-    res.status(500).send({ message: err.message })
+    res.status(500).send({ message: err.message });
   }
 };
 
-module.exports = { login };
+const motherRegister = async (req, res) => {
+  const { idCard, firstName, lastName, phoneNumber, email } = req.body;
+  const targetIdCard = await db.MotherProfile.findOne({ where: { idCard } });
+  if (targetIdCard) {
+    res.status(400).send({ message: 'Username already taken.' });
+  } else {
+    const salt = bcryptjs.genSaltSync(Number(process.env.SALT_ROUND));
+    const hashedPassword = bcryptjs.hashSync(idCard, salt);
+    const newMotherProfile = await db.MotherProfile.create({
+      idCard,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).send(newMotherProfile);
+  }
+};
+
+const createCurrentPregnancy = (req, res) => {
+  res.send();
+};
+
+module.exports = {
+  login,
+  motherRegister,
+  createCurrentPregnancy,
+};
--- routes/staff.js
+++ routes/staff.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { login } = require('../controllers/staff');
+const { login, motherRegister, createCurrentPregnancy } = require('../controllers/staff');
 
 router.post('/login', login);
+router.post('/motherRegister', motherRegister);
+router.post('/createCurrentPregnancy', createCurrentPregnancy);
 
 module.exports = router;

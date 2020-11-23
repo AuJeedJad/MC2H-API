76a3f1555b9a6132e8b52f424843e8fd2d903d08
koidzt <koi27792@gmail.com>
2020-11-23 15:09:52 +0700

    add try-catch into motherRegister

--- controllers/staff.js
+++ controllers/staff.js
@@ -26,23 +26,27 @@ const login = async (req, res) => {
 };
 
 const motherRegister = async (req, res) => {
-  const { idCard, firstName, lastName, phoneNumber, email } = req.body;
-  const targetIdCard = await db.MotherProfile.findOne({ where: { idCard } });
-  if (targetIdCard) {
-    res.status(400).send({ message: 'Username already taken.' });
-  } else {
-    const salt = bcryptjs.genSaltSync(Number(process.env.SALT_ROUND));
-    const hashedPassword = bcryptjs.hashSync(idCard, salt);
-    const newMotherProfile = await db.MotherProfile.create({
-      idCard,
-      firstName,
-      lastName,
-      phoneNumber,
-      email,
-      password: hashedPassword,
-    });
+  try {
+    const { idCard, firstName, lastName, phoneNumber, email } = req.body;
+    const targetIdCard = await db.MotherProfile.findOne({ where: { idCard } });
+    if (targetIdCard) {
+      res.status(400).send({ message: 'Username already taken.' });
+    } else {
+      const salt = bcryptjs.genSaltSync(Number(process.env.SALT_ROUND));
+      const hashedPassword = bcryptjs.hashSync(idCard, salt);
+      const newMotherProfile = await db.MotherProfile.create({
+        idCard,
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        password: hashedPassword,
+      });
 
-    res.status(201).send(newMotherProfile);
+      res.status(201).send(newMotherProfile);
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
 };
 

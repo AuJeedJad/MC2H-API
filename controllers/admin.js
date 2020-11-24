5f97a8eb7776b0bd327216b385bc0e040efa710a
koidzt <koi27792@gmail.com>
2020-11-24 13:20:09 +0700

    validate staffRegister,resetStaffPassword

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,17 +4,26 @@ const bcryptjs = require('bcryptjs');
 const register = async (req, res) => {
   try {
     const { username, password, secretKey } = req.body;
-    const isCorrect = await bcryptjs.compareSync(secretKey, process.env.HASHED_SECRET_KEY);
-    if (!isCorrect) {
-      res.status(400).send({ message: 'Bad request' });
+    if (!username) {
+      return res.status(400).send({ message: 'กรุณาใส่ username' });
     }
-
+    if (!password) {
+      return res.status(400).send({ message: 'กรุณาใส่ password' });
+    }
+    if (!secretKey) {
+      return res.status(400).send({ message: 'กรุณาใส่รหัส admin' });
+    } else {
+      const isCorrect = await bcryptjs.compareSync(secretKey, process.env.HASHED_SECRET_KEY);
+      if (!isCorrect) {
+        return res.status(400).send({ message: 'รหัสไม่ถูกต้อง' });
+      }
+    }
     const targetUser = await db.Staff.findOne({ where: { username } });
     if (targetUser) {
-      res.status(400).send({ message: 'Username already taken.' });
+      res.status(400).send({ message: 'Username นี้มีในระบบแล้ว' });
     } else {
       if (password.length < 6) {
-        res.status(400).send({ message: 'Please enter password more than 6 character' });
+        res.status(400).send({ message: 'กรุณาใส่รหัสผ่านอย่างน้อย 6 ตัวอักษร' });
       } else {
         const salt = bcryptjs.genSaltSync(Number(process.env.SALT_ROUND));
         const hashedPassword = bcryptjs.hashSync(password, salt);
@@ -33,20 +42,35 @@ const register = async (req, res) => {
 
 const resetPassword = async (req, res) => {
   try {
-    const { username, setPassword } = req.body;
-    if (setPassword.length < 6) {
-      return res.status(400).send({ message: 'Please enter password more than 6 character' });
+    const { username, setPassword, secretKey } = req.body;
+    if (!username) {
+      return res.status(400).send({ message: 'กรุณาใส่ username ที่ต้องการเปลี่ยนรหัส' });
+    }
+    if (!setPassword) {
+      return res.status(400).send({ message: 'กรุณาใส่รหัสที่ต้องการตั้งใหม่' });
+    } else {
+      if (setPassword.length < 6) {
+        return res.status(400).send({ message: 'กรุณาใส่รหัสผ่านอย่างน้อย 6 ตัวอักษร' });
+      }
+    }
+    if (!secretKey) {
+      return res.status(400).send({ message: 'กรุณาใส่รหัส admin' });
+    } else {
+      const isCorrect = await bcryptjs.compareSync(secretKey, process.env.HASHED_SECRET_KEY);
+      if (!isCorrect) {
+        return res.status(400).send({ message: 'รหัสไม่ถูกต้อง' });
+      }
     }
 
     const targetUser = await db.Staff.findOne({ where: { username } });
     if (!targetUser) {
-      res.status(400).send({ message: 'This username is not found' });
+      res.status(400).send({ message: 'ไม่มี username นี้ในระบบ' });
     } else {
       const salt = bcryptjs.genSaltSync(Number(process.env.SALT_ROUND));
       const hashedPassword = bcryptjs.hashSync(setPassword, salt);
       await targetUser.update({ password: hashedPassword });
 
-      res.status(200).send({ message: 'Password has been reset' });
+      res.status(200).send({ message: 'เปลี่ยนรหัสผ่านเรียบร้อยแล้ว' });
     }
   } catch (err) {
     res.status(500).send({ message: err.message });

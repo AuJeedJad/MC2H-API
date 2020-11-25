a56363f03067d62456d1c7489ff96d2127e2ace2
Tanapat-P <jeenohu@hotmail.com>
2020-11-25 11:11:56 +0700

    fix index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const adminRoute = require('./routes/admin');
 const staffRoute = require('./routes/staff');
 const motherProfileRoute = require('./routes/motherProfile');
 const motherRoute = require('./routes/mother');
+const staticRiskEvaluationRoute = require('./routes/staticRiskEvaluation');
+const RiskEvaluationRoute = require('./routes/RiskEvaluation');
+const DentalRoute = require('./routes/Dental');
 
 app.use(cors());
 app.use(express.json());
@@ -20,11 +23,16 @@ app.use('/staff', staffRoute);
 app.use('/motherProfile', motherProfileRoute);
 app.use('/mother', motherRoute);
 
+app.use('/staticRiskEvaluation', staticRiskEvaluationRoute);
+app.use('/RiskEvaluation', RiskEvaluationRoute);
+app.use('/Dental', DentalRoute);
+
 app.use((req, res, next) => {
   res.status(404).send({ message: 'Not Found' });
 });
 
 app.use((err, req, res, next) => {
+  console.log(err);
   res.status(500).send({ message: err.message });
 });
 

62d2f1fbc741a8ca47c660a40d2a1283c34ad1f2
ZalotBabyZ <babyzalot@gmail.com>
2020-11-28 01:13:11 +0700

    mother find id card

--- controllers/motherAccount.js
+++ controllers/motherAccount.js
@@ -1,6 +1,28 @@
 const db = require('../models');
 const bcryptjs = require('bcryptjs');
 
+const motherFind = async (req, res) => {
+  try {
+    const idCard = req.query.idCard;
+    if (!idCard) {
+      return res.status(400).send({ message: 'ID card not exist' });
+    }
+    if (idCard.length !== 13 || isNaN(idCard)) {
+      return res.status(400).send({ message: 'Incorrect ID card format' });
+    }
+
+    const targetIdCard = await db.MotherProfile.findOne({ where: { idCard } });
+
+    if (!targetIdCard) {
+      res.status(404).send({ message: 'Non subscribe ID card number' });
+    } else {
+      res.status(200).send(targetIdCard);
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
 const motherRegister = async (req, res) => {
   try {
     const { idCard, firstName, lastName, phoneNumber } = req.body;
@@ -15,6 +37,7 @@ const motherRegister = async (req, res) => {
 
     if (targetIdCard) {
       res.status(400).send({ message: 'IdCard: ' + idCard + ' already exist.' });
+      // const CurrentPreg =await db.CurrentPregnancy.findOne({ where: { idCard } });
     } else {
       if (!firstName) {
         return res.status(400).send({ message: 'First name not exist.' });
@@ -68,6 +91,7 @@ const createCurrentPregnancy = async (req, res) => {
 };
 
 module.exports = {
+  motherFind,
   motherRegister,
   createCurrentPregnancy,
 };
--- routes/motherAccount.js
+++ routes/motherAccount.js
@@ -1,9 +1,10 @@
 const passport = require('passport');
 const router = require('express').Router();
-const { motherRegister, createCurrentPregnancy } = require('../controllers/motherAccount');
+const { motherRegister, createCurrentPregnancy, motherFind } = require('../controllers/motherAccount');
 
 const authStaff = passport.authenticate('jwt-staff', { session: false });
 
+router.get('/motherFind', authStaff, motherFind);
 router.post('/motherRegister', authStaff, motherRegister);
 router.post('/createCurrentPregnancy', authStaff, createCurrentPregnancy);
 

bfc9f61fff6d9b576a1af16fa99234e5f0881269
BallStripz <infinitivea@gmail.com>
2020-12-04 19:54:28 +0700

    Do FatherProfile

--- controllers/fatherProfile.js
+++ controllers/fatherProfile.js
@@ -2,7 +2,7 @@ const db = require('../models');
 
 const findFather = async (req, res, next) => {
   try {
-    const { curPregId } = req.body;
+    const { curPregId } = req.query;
     const targetCurPreg = await db.CurrentPregnancy.findOne({ where: curPregId });
     const targetFather = await db.FatherProfile.findOne({ where: targetCurPreg.id });
 
